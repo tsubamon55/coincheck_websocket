70fbfa8669f38ab473c9adc02bf04853006d2972
tsubamon55 <124486733+tsubamon55@users.noreply.github.com>
2023-12-02 23:44:52 +0900

    Delete settings.py

--- settings.py
+++
@@ -1,6 +0,0 @@
-username = 'coincheck'
-password = '4AT07MjDsW280S_yzAYLrj1u9bnuGKupVxVt0Z95OcA'  # @coincheck
-host = '157.7.212.250'
-db_name = 'coincheck'
-
-websocket_url = "wss://ws-api.coincheck.com/"
